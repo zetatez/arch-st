9e8d4e37ed57a3e96480c5515a83a16209658895
zetatez <zetatez@icloud.com>
2022-07-02 09:19:36 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,8 +94,8 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-// float alpha = 0.64, alphaUnfocused = 0.48;
-float alpha = 0.72, alphaUnfocused = 0.64;
+// float alpha = 0.72, alphaUnfocused = 0.64;
+float alpha = 0.80, alphaUnfocused = 0.16;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
