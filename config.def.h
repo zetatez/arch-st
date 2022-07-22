a7f785b2528c53292a3b7dfcad2a82e22b37773d
zetatez <zetatez@icloud.com>
2022-07-22 17:35:58 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -95,7 +95,8 @@ unsigned int tabspaces = 4;
 
 /* bg opacity */
 // float alpha = 0.72, alphaUnfocused = 0.64;
-float alpha = 0.64, alphaUnfocused = 0.32;
+// float alpha = 0.81, alphaUnfocused = 0.48;
+float alpha = 0.68, alphaUnfocused = 0.32;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -137,21 +138,18 @@ unsigned int defaultbg = 0;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 unsigned int bg = 0, bgUnfocused = 0;                                                                                                    // st-focus
-
-unsigned int const currentBg = 6, buffSize = 2048;                                                                                       // st-meta-vim-full
-/// Enable double / triple click yanking / selection of word / line.                                                                     // st-meta-vim-full
-int const mouseYank = 1, mouseSelect = 1;                                                                                                // st-meta-vim-full
-/// [Vim Browse] Colors for search results currently on screen.                                                                          // st-meta-vim-full
-unsigned int const highlightBg = 10, highlightFg = 0;                                                                                    // st-meta-vim-full
-char const wDelS[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", wDelL[] = " \t";                                                               // st-meta-vim-full
-char *nmKeys [] = {              ///< Shortcusts executed in normal mode                                                                 // st-meta-vim-full
-  "R/Building\nN", "r/Building\n", "X/juli@machine\nN", "x/juli@machine\n",                                                              // st-meta-vim-full
-  "Q?[Leaving vim, starting execution]\n","F/: error:\nN", "f/: error:\n", "DQf"                                                         // st-meta-vim-full
-};                                                                                                                                       // st-meta-vim-full
-unsigned int const amountNmKeys = sizeof(nmKeys) / sizeof(*nmKeys);                                                                      // st-meta-vim-full
-/// Style of the {command, search} string shown in the right corner (y,v,V,/)                                                            // st-meta-vim-full
-Glyph styleSearch = {' ', ATTR_ITALIC | ATTR_BOLD_FAINT, 7, 16};                                                                         // st-meta-vim-full
-Glyph style[] = {{' ',ATTR_ITALIC|ATTR_FAINT,15,16}, {' ',ATTR_ITALIC,232,11}, {' ', ATTR_ITALIC, 232, 4}, {' ', ATTR_ITALIC, 232, 12}}; // st-meta-vim-full
+unsigned int const currentBg = 6, buffSize = 2048;                                                                                                                              // st -meta-vim-full
+/// Enable double / triple click yanking / selection of word / line.                                                                                                            // st -meta-vim-full
+int const mouseYank = 1, mouseSelect = 1;                                                                                                                                       // st -meta-vim-full
+/// [Vim Browse] Colors for search results currently on screen.                                                                                                                 // st -meta-vim-full
+unsigned int const highlightBg = 10, highlightFg = 0;                                                                                                                           // st -meta-vim-full
+char const wDelS[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", wDelL[] = " \t";                                                                                                      // st -meta-vim-full
+ //< Shortcusts executed in normal mode                                                                                                                                         // st -meta-vim-full
+char *nmKeys [] = { "R/Building\nN", "r/Building\n", "X/juli@machine\nN", "x/juli@machine\n", "Q?[Leaving vim, starting execution]\n","F/: error:\nN", "f/: error:\n", "DQf" }; // st -meta-vim-full
+unsigned int const amountNmKeys = sizeof(nmKeys) / sizeof(*nmKeys);                                                                                                             // st -meta-vim-full
+/// Style of the {command, search} string shown in the right corner (y,v,V,/)                                                                                                   // st -meta-vim-full
+Glyph styleSearch = {' ', ATTR_ITALIC | ATTR_BOLD_FAINT, 7, 16};                                                                                                                // st -meta-vim-full
+Glyph style[] = {{' ',ATTR_ITALIC|ATTR_FAINT,15,16}, {' ',ATTR_ITALIC,232,11}, {' ', ATTR_ITALIC, 232, 4}, {' ', ATTR_ITALIC, 232, 12}};                                        // st -meta-vim-full
 
 /*
  * Default shape of cursor
